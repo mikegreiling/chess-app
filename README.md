fd5299b77847a5fe69054137eb15b80a5f0058ef
Mike Greiling <mike@pixelcog.com>
2023-10-25 01:03:16 -0500

    Add some more functionality

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This app defines a simple hook for chess.js which allows a React
 component to access and respond to mutations in the state of a game
 of chess.
+
+The `useChessGame` hook instantiates an extended `chess.js` object
+which runs a callback on each method which mutates its state. We use
+this in conjunction with `useState` internally to essentially make
+the chess object "reactive".
+
+The `useChessGame` returns a tuple consisting of the chess game
+object, the current FEN state, and the current PGN state.
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,78 @@
 import "./App.css";
 import { Chessboard } from "react-chessboard";
 import useChessGame from "./hooks/useChessGame";
-import type { Chess } from "chess.js";
+import type { Chess, Square } from "chess.js";
 
-const getGameStatus = (chess: Chess) => {
-  if (chess.isGameOver()) {
-    if (chess.isCheckmate()) {
+function getGameStatus(game: Chess) {
+  if (game.isGameOver()) {
+    if (game.isCheckmate()) {
       return "Checkmate!";
-    } else if (chess.isDraw()) {
+    } else if (game.isDraw()) {
       return "Draw!";
-    } else if (chess.isStalemate()) {
+    } else if (game.isStalemate()) {
       return "Stalemate!";
     } else {
       return "Game Over!";
     }
   } else {
-    if (chess.turn() === "w") {
+    if (game.turn() === "w") {
       return "White's Turn";
     } else {
       return "Black's Turn";
     }
   }
-};
+}
+
+// Converts react-chessboard Piece ('wQ') to chess.js PieceSymbol ('q')
+function getPieceSymbol(piece: string) {
+  if (piece.length > 1) {
+    return piece[1].toLowerCase();
+  }
+  return undefined;
+}
 
 function App() {
-  const [chess, fen] = useChessGame("example-game", (chess) => chess.reset());
+  const [game, fen] = useChessGame("example-game", (game) => game.reset());
+
+  const makeMove = (
+    sourceSquare: Square,
+    targetSquare: Square,
+    piece: string
+  ) => {
+    try {
+      game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: getPieceSymbol(piece),
+      });
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
 
   const makeRandomMove = () => {
-    if (!chess.isGameOver()) {
-      const moves = chess.moves();
+    if (!game.isGameOver()) {
+      const moves = game.moves();
       const move = moves[Math.floor(Math.random() * moves.length)];
-      chess.move(move);
+      game.move(move);
     }
+  };
+
+  const resetGame = () => {
+    game.reset();
   };
 
   return (
     <>
       <h1>Basic Chess App</h1>
-      <strong>Status: {getGameStatus(chess)}</strong>
-      <Chessboard position={fen} />
+      <strong>Status: {getGameStatus(game)}</strong>
+      <Chessboard position={fen} onPieceDrop={makeMove} />
       <br />
-      <button onClick={makeRandomMove} disabled={chess.isGameOver()}>
+      <button onClick={makeRandomMove} disabled={game.isGameOver()}>
         Random Move
-      </button>
+      </button>{" "}
+      <button onClick={resetGame}>Reset Game</button>
     </>
   );
 }
